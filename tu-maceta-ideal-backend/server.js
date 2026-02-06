d0adf91f0be767b9c713dcd9efcefce9dff2d831
aldanaisabel <calvoaldana95@gmail.com>
2026-02-05 21:30:07 -0300

    Corregido ReferenceError: definido mongoose

--- tu-maceta-ideal-backend/server.js
+++ tu-maceta-ideal-backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoURI = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
